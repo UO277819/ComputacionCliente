a269f6e2e8009a17311413cb126ef692c1250b5f
Aaron Garcia Garcia <90830415+UO277819@users.noreply.github.com>
2022-12-06 19:11:10 +0100

    Add files via upload

--- Ejercicio11.js
+++ Ejercicio11.js
@@ -5,25 +5,32 @@ class GeoLocalizacion{
         this.imagenMapa="";
         this.latitud=0;
         this.longitud=0;
-    }
-
-    mostrar(){
         navigator.geolocation.getCurrentPosition(this.getPosition.bind(this),this.mostrarErrores.bind(this));
     }
 
     getPosition(posicion){
-        var datos=''; 
+        var datos='';
+        datos+='<section><h2>Datos</h2>';  
         datos+='<p>Longitud: '+posicion.coords.longitude +' grados</p>'; 
         datos+='<p>Latitud: '+posicion.coords.latitude +' grados</p>';
         datos+='<p>Precisión de la latitud y longitud: '+ posicion.coords.accuracy +' metros</p>';
         datos+='<p>Altitud: '+ posicion.coords.altitude +' metros</p>';
         datos+='<p>Precisión de la altitud: '+ posicion.coords.altitudeAccuracy +' metros</p>'; 
         datos+='<p>Rumbo: '+ posicion.coords.heading +' grados</p>'; 
-        datos+='<p>Velocidad: '+ posicion.coords.speed +' metros/segundo</p>';
-        $("section").html(datos);
+        datos+='<p>Velocidad: '+ posicion.coords.speed +' metros/segundo</p></section>';
+        $("section").after(datos);
         this.longitud=posicion.coords.longitude;
         this.latitud=posicion.coords.latitude;
-        this.getMapaEstaticoGoogle();
+        mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaWV0ZTE3IiwiYSI6ImNrd2J5c2JyeTFndDMydXFtYjMweXFpcWEifQ.0SKmdulKoTv4TGHbrJWvAg';  
+        var map = new mapboxgl.Map({ 
+            container: 'map', 
+            style: 'mapbox://styles/mapbox/streets-v10',  
+            center: [this.longitud, this.latitud],
+            zoom: 13
+            }); 
+        //var marker = new mapboxgl.Marker().setLngLat([this.longitud, this.latitud]).addTo(map);
+        //var mapaa=$("section");
+       //$("h1").after(mapaa);
     }
 
     mostrarErrores(error){
@@ -38,19 +45,6 @@ class GeoLocalizacion{
             this.mensaje = "Se ha producido un error desconocido"
         }
         $("h1").html(this.mensaje);
-    }
-
-    getMapaEstaticoGoogle(){
-        var apiKey = "&key=AIzaSyC6j4mF6blrc4kZ54S6vYZ2_FpMY9VzyRU";
-        var url = "https://maps.googleapis.com/maps/api/staticmap?";
-        var centro = "center=" + this.latitud + "," + this.longitud;
-        var zoom ="&zoom=15";
-        var tamaño= "&size=800x600";
-        var marcador = "&markers=color:red%7Clabel:S%7C" + this.latitud + "," + this.longitud;
-        var sensor = "&sensor=false"; 
-        
-        this.imagenMapa = url + centro + zoom + tamaño + marcador + sensor + apiKey;
-        $("section").after("<img src='"+this.imagenMapa+"' alt='mapa estático google' />");
     }
 
 }
