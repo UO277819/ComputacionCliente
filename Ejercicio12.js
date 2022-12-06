5d69cf43ada8be120d38b52da231584b00d7f102
Aaron Garcia Garcia <90830415+UO277819@users.noreply.github.com>
2022-12-06 17:06:48 +0100

    Add files via upload

---
+++ Ejercicio12.js
@@ -0,0 +1,33 @@
+"use strict";
+class Analizador {
+    constructor(){
+    }
+    obtenerDatos(){
+        $('h3').remove();
+        $('p').remove();
+        $('textarea').remove();
+        var listaArchivos = $('input[type=file]');
+        for (var i = 0; i < listaArchivos.length; i++) {
+            var nBytes = 0,
+            archivos=listaArchivos.get(i).files,
+            nArchivos = archivos.length;
+                for (var j = 0; j < nArchivos; j++) {
+                    var reader = new FileReader();
+                    $("form").after("<h3>Nombre de archivo: " + archivos[j].name + "</h3>"
+                        + "<p>Tipo de archivo: " + archivos[j].type + "</p>" +
+                        "<p>Tamaño de archivo: " + archivos[j].size + "</p>");
+                    console.log(archivos[j].type);
+                    if (archivos[j].type == 'application/json' || archivos[j].type == 'text/xml' || archivos[j].type == 'text/plain') {
+                        reader.onload = function (progressEvent) {
+                        $("footer").before("<textarea name = 'texto' cols = '120' rows = '50' disabled>" + this.result + "</textarea>");
+                        };
+                    }
+                    reader.readAsText(archivos[j]);
+                    
+
+                    nBytes += archivos[j].size;
+                }
+        }
+    }
+}
+var archivo = new Analizador();
