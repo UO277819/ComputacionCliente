9b5ec1800e7f5f18d4fcd4a5d8cd4606005b1d77
Aaron Garcia Garcia <90830415+UO277819@users.noreply.github.com>
2022-12-06 18:30:20 +0100

    Update Ejercicio11.js

--- Ejercicio11.js
+++ Ejercicio11.js
@@ -1,15 +1,18 @@
 "use strict";
 class GeoLocalizacion{
     constructor(){
+        this.mensaje="";
+        this.imagenMapa="";
+        this.latitud=0;
+        this.longitud=0;
     }
 
     mostrar(){
-        navigator.geolocation.getCurrentPosition(this.getPosition.bind(this));
+        navigator.geolocation.getCurrentPosition(this.getPosition.bind(this),this.mostrarErrores.bind(this));
     }
 
     getPosition(posicion){
         var datos=''; 
-        datos+='<h2>Datos</h2>';
         datos+='<p>Longitud: '+posicion.coords.longitude +' grados</p>'; 
         datos+='<p>Latitud: '+posicion.coords.latitude +' grados</p>';
         datos+='<p>Precisión de la latitud y longitud: '+ posicion.coords.accuracy +' metros</p>';
@@ -18,6 +21,37 @@ class GeoLocalizacion{
         datos+='<p>Rumbo: '+ posicion.coords.heading +' grados</p>'; 
         datos+='<p>Velocidad: '+ posicion.coords.speed +' metros/segundo</p>';
         $("section").html(datos);
+        this.longitud=posicion.coords.longitude;
+        this.latitud=posicion.coords.latitude;
+        this.getMapaEstaticoGoogle();
     }
+
+    mostrarErrores(error){
+        switch(error.code) {
+        case error.PERMISSION_DENIED:
+            this.mensaje = "El usuario no permite la petición de geolocalización"
+        case error.POSITION_UNAVAILABLE:
+            this.mensaje = "Información de geolocalización no disponible"
+        case error.TIMEOUT:
+            this.mensaje = "La petición de geolocalización ha caducado"
+        case error.UNKNOWN_ERROR:
+            this.mensaje = "Se ha producido un error desconocido"
+        }
+        $("h1").html(this.mensaje);
+    }
+
+    getMapaEstaticoGoogle(){
+        var apiKey = "&key=AIzaSyC6j4mF6blrc4kZ54S6vYZ2_FpMY9VzyRU";
+        var url = "https://maps.googleapis.com/maps/api/staticmap?";
+        var centro = "center=" + this.latitud + "," + this.longitud;
+        var zoom ="&zoom=15";
+        var tamaño= "&size=800x600";
+        var marcador = "&markers=color:red%7Clabel:S%7C" + this.latitud + "," + this.longitud;
+        var sensor = "&sensor=false"; 
+        
+        this.imagenMapa = url + centro + zoom + tamaño + marcador + sensor + apiKey;
+        $("section").after("<img src='"+this.imagenMapa+"' alt='mapa estático google' />");
+    }
+
 }
 var localizacion=new GeoLocalizacion();
